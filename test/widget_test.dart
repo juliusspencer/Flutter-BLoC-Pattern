344ba09c7b7924074efd969cc0760e2d123037dd
Julius Spencer <jspencer@juliusspencer.co.nz>
2018-08-28 11:52:18 +1200

    Updated test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,19 +11,27 @@ import 'package:flutter_bloc_pattern/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+
     await tester.pumpWidget(new MyApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Items: 0'), findsOneWidget);
+    expect(find.text('Items: 1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Items: 0'), findsNothing);
+    expect(find.text('Items: 1'), findsOneWidget);
+
+    // Tap the '-' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.remove));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('Items: 0'), findsOneWidget);
+    expect(find.text('Items: 1'), findsNothing);
   });
 }
